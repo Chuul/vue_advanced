ff0bf5f3980c85729a3ebce758f66ca399355680
Chuul <bethejs30@gmail.com>
2021-07-26 16:30:44 +0900

    until Refactoring1_styling

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-      <p v-for="item in this.fetchedAsk" v-bind:key=item.title>
-        <router-link v-bind:to="`/item/${item.id}`">{{ item.title }}</router-link>
-        <small>{{ item.time_ago }} by {{ item.user }}</small>
-      </p>
+    <ul class="ask-list">
+      <li v-for="item in fetchedAsk" v-bind:key=item.title class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="ask-title">
+           <router-link v-bind:to="`/item/${item.id}`">{{ item.title }}</router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by  
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
+              "{{item.user}}"
+            </router-link> 
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,6 +38,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.ask-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style : none;
+  display: flex;
+  align-items: center;
+  border-bottom : 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.ask-title {
+  margin : 0;
+}
+.link-text {
+  color : #828282
+}
 </style>
